63cc7bc82f25abd73de0b1a2ce4bab526d3addb1
yassin-x <yassin.ibrahim.hamed@gmail.com>
2025-08-22 12:05:55 +0300

    floosyId

--- src/lib/floosyId.ts
+++ src/lib/floosyId.ts
@@ -1,10 +1,10 @@
 export default function generateFloosyId(): string {
-  const floosyEpoch = BigInt(1753929600000); // 2025-08-01T00:00:00Z
+  const floosyEpoch = BigInt(1753929600000);
   const timestamp = BigInt(Date.now()) - floosyEpoch;
 
   const machineId = BigInt(1);
   const processId = BigInt(1);
-  const increment = BigInt(Math.floor(Math.random() * 4096)); // 12 bit
+  const increment = BigInt(Math.floor(Math.random() * 4096));
 
   const floosyflake =
     (timestamp << BigInt(22)) |
